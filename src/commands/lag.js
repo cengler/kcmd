0cdb3dbd535705ceaf46b27cff756a469df4f969
Christian Engler <christian.engler@despegar.com>
2022-03-01 01:06:40 -0300

    rem lag console log

--- src/commands/lag.js
+++ src/commands/lag.js
@@ -6,7 +6,6 @@ import configUtils from "../util/configUtils";
 async function lag() {
   const sk = configUtils.getKafka()
   const group = configUtils.getGroup()
-
   const gOffsets = await kafka.groupOffsets(sk.brokers, group)
   const topics = _.uniq(gOffsets.map(go => go.topic))
   const tOffsets = _.flatten(
@@ -14,18 +13,12 @@ async function lag() {
       topics.map(topic => kafka.topicOffsets(sk.brokers, topic)
         .then(tos => tos.map(to => ({topic, ...to}))))
     ))
-
   const gOffsetsMap = new Map(gOffsets.map(i => [`${i.topic}-${i.partition}`, i.offset]));
-
   const lags = tOffsets.map(({topic, partition, offset}) => {
     const lag = Number.parseInt(offset) - Number.parseInt(gOffsetsMap.get(`${topic}-${partition}`))
     return {topic, partition, lag}
   })
-
-  console.log(group)
-  console.log(gOffsetsMap)
   display.print(lags)
-
 }
 
 module.exports = lag
